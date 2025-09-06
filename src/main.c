3a52cb6cb47a141161283596d65cae347c56ecc3
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2025-09-06 17:55:22 -0300

    feat: add complex functions

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <math.h>
+
+double f1(double x) {
+    return (tan(pow(x, 3) * cos(x) * (3 * log(x) + sin(x)))) / (pow(x, 4) * exp(3 * x + 5));
+}
+
+double f2(double x) {
+    return (pow(x, 3) * sin(log(1 + pow(x, 2) + pow(x, 8))))/ ((pow(x, 2) + 1) * sqrt(1 + pow(x, 2)));
+}
 
 int main () {
 
-    printf("Hello WMMA!\n");
+    printf("%.2f\n", f1(0.01));
+    printf("%.2f\n", f2(0.01));
 
     return 0;
 }
